74678b9c5f1802c3dbf8c280c5cdf94a243cf003
Tranvanhien141625 <128828849+Tranvanhien141625@users.noreply.github.com>
2024-05-20 09:16:32 +0700

    Update App_dinh_gia_nha.py

--- App_dinh_gia_nha.py
+++ App_dinh_gia_nha.py
@@ -5,25 +5,28 @@ import pandas as pd
 import joblib
 from pydantic import BaseModel
 
-class Dinhgia(BaseModel):
-    Diện tích: float
-    Số phòng ngủ: float
-    Số phòng toilet: float
-    Số tầng: float
-    Giấy_tờ_pháp_lý: str
-    Nội thất: str
-    Đường: str
-    Tỉnh: str
-    Quận: str
-    Phường: str
 
-#numerical_cols = ['Diện tích', 'Số phòng ngủ', 'Số phòng toilet', 'Số tầng']
-# categorical_cols = ['balcony_direction',	'house_direction',	'property_legal_document',	'furniture','Đường','Tỉnh',	'Quận',	'Phường']  # thay thế các giá trị thực tế của bạn
-#categorical_cols = ['Kiểu nhà','Giấy tờ pháp lý','Nội thất','Đường','Tỉnh',	'Quận',	'Phường']  # thay thế các giá trị thực tế của bạn
+#area Diện tích
+# way_in: Chiều rộng ngõ
+#frontage: Mặt tiền
+
+class Dinhgia(BaseModel):
+    area: float
+    way_in: float
+    frontage: float
+    bedroom: float
+    toilet: float
+    floors: float
+    Phường: str
+    Quận: str
+    Tỉnh: str
+    
+
+
 
 app = FastAPI()
 # Load mô hình đã lưu, và đảm bảo dấu ngoặc đóng chính xác
-model = joblib.load('c:\Users\admin\Downloads\model_nha_ơ_nha_tot_Ha_Noi')
+model = joblib.load('C:\Users\admin\Downloads\APP_nha_o\model_nha_mat_pho_khong_ten_duong.pkl')
 
 
 @app.get('/')
